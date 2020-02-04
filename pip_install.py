cf179c6f8f48a750ba6eb1f0526ab7df6fb74e97
Thomas Kavanagh <thomaskavanagh.dev@gmail.com>
2020-02-04 17:37:46 -0500

    Add list of pip install commands from Colab

---
+++ pip_install.py
@@ -0,0 +1,9 @@
+!pip install pystac
+!pip install geopandas
+!pip install rasterio
+!apt-get install python3-rtree
+!pip install -q git+https://github.com/tensorflow/examples.git
+!pip install tensorflow -U
+!apt-get install -y libsm6 libxext6 libxrender-dev
+!pip install opencv-python
+!pip install keras-segmentation
